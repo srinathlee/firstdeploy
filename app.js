92553d678ff7dd8b15959273b9a3728942ec09e8
srinath <iamsrinath5255@gmail.com>
2022-11-29 20:16:15 +0530

    added db

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+const { response } = require("express");
 const express=require("express");
 const app=express();
+const mysql=require("mysql");
+
+const connection=mysql.createConnection({
+    host:"localhost",
+    user:"db_user",
+    password:"password",
+    database:"mysql://root:EgLAg7ByaBgWyPCoTuWb@containers-us-west-98.railway.app:7325/railway"
+})
+connection.connect();
 
 app.get("/",(request,response)=>{
-    response.send("this is home page");
+    connection.query("select * from cricket_team",(error,result)=>{
+        if (error){
+            response.send(error)
+        }
+        response.send(result)
+    })
+    
+   // response.send("this is home page");
 })
 
 app.get("/about/",(request,response)=>{
@@ -10,4 +27,4 @@ app.get("/about/",(request,response)=>{
 })
 
 
-app.listen(process.env.PORT || 3000,()=>{console.log("app is started at port 3000")})
+app.listen(process.env.PORT || 5820,()=>{console.log("app is started at port 3000")})
